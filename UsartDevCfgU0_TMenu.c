e0743d76019bbc2a63ec3f28e947f94d08fe8d8f
thtfcccj <thtfcccj@qq.com>
2022-04-05 21:06:59 +0800

    1. 优化CfgUser0结构，增加工作模式，协议与协议参数段，以及成员顺序 2. 设计增加UsartDevCfg在使用TMenu菜单系统时的实现 3. 设计增加UsartDevCfgUser0模式下，在使用TMenu菜单系统时的实现
    上述未调试与测试

---
+++ UsartDevCfgU0_TMenu.c
@@ -0,0 +1,145 @@
+/***************************************************************************
+
+     UsartDevCfg扩展用户配置->方式0,-在使用TMenu菜单系统时的实现
+                         
+****************************************************************************/
+
+#include "UsartDevCfg_TMenu.h"
+#include "TMenu_MNumAdj.h"
+#include "UiTips.h"
+#include "Power.h"
+
+#include "stringEx.h"
+#include <string.h>
+#include "TMenuBuf.h"
+
+//--------------------------------内部字符资源--------------------------------- 
+#ifdef TM_EN_MUTI_LAN            //常量选择型多国语言时
+  //暂不支持常量选择型字符串
+#elif defined(TM_RESOURCE_LAN)  //由资源文件获取多国语言时
+  //菜单
+  #include "lsAry.h"
+  //提示
+  #include "Rid.h"
+  #include "RidNote.h"
+  #define ls_OpFinal   RID_pGeNote(RID_NOTE_OP_FINAL)
+#else  //单一语言字符串时
+  //菜单
+  extern const char ls_WorkMode[]      = {"工作模式"};
+  extern const char ls_Protocol[]      = {"通讯协议"};
+  extern const char ls_ProtocolPara[]  = {"协议参数"};
+  extern const char ls_SpaceT[]        = {"帧间隔时间"};
+  extern const char ls_WaitT[]         = {"接收等待时间"};
+  extern const char ls_SlaveAdr[]      = {"从机地址"};
+  extern const char ls_WaitRouter[]    = {"等待路由时间"}; 
+  
+  extern const char ls_WorkParaSet[]   = {"工作参数设置"};  
+  
+  //提示
+  extern const char ls_OpFinal[]       = {"操作成功!"};
+#endif
+
+//-----------------------------------查找表--------------------------------
+//子菜单查找表结构
+static const unsigned char _MasterSuper[] = //主机模式超级用户
+{5,    0,1,2,3,4};
+static const unsigned char _SlaveSuper[] = //从机模式超级用户
+{5,    5,0,1,2,6};
+
+static const unsigned char _MasterAdmin[] = //主机模式管理员
+{2,    3,4};
+static const unsigned char _SlaveAdmin[] = //从机模式管理员
+{1,    5};
+
+//为管理员模式查找表掩码
+extern unsigned short UsartDevCfgU0_cbIsMasterMask;
+
+//----------------------------------回调函数------------------------------- 
+//描述
+static const struct _MNumDesc _Desc[] = {
+  {MNUM_TYPE_DEC, 0, 15},//工作模式
+  {MNUM_TYPE_DEC, 0, 3},//通讯协议
+  {MNUM_TYPE_DEC, 0, 3},//参数配置
+  {MNUM_TYPE_DEC, 0, 255},//帧间隔时间
+  {MNUM_TYPE_DEC, 10,255},//接收等待时间
+  {MNUM_TYPE_DEC, 1, 255},//从机地址
+  {MNUM_TYPE_DEC, 20,255},//路由等待时间
+};
+
+static void _Notify(unsigned char Type,//通报类型
+                    //根据菜单类型输入的输入输出数据区
+                    //该区域结构在控件里定义,直接指针转换即可
+                    void *pv)
+{
+  struct _UsartDevCfg *pCfg = &UsartDevCfg[UsartDevCfgTMenu_cbGetId()];
+  struct _MNumAdjUser *pUser = (struct _MNumAdjUser *)pv;
+  
+  switch(Type){
+  case TM_NOTIFY_GET_DATA:{ //将当前值装入
+    pUser->Value[0] = pCfg->U.M.Cfg >> 4;
+    pUser->Value[1] = (pCfg->U.M.Cfg >> 2) & 0x03;  
+    pUser->Value[2] = pCfg->U.M.Cfg & 0x03;   
+    pUser->Value[3] = pCfg->U.M.SpaceT;//帧间隔时间
+    pUser->Value[4] = pCfg->U.M.WaitT; //接收等待时间
+    pUser->Value[5] = pCfg->U.S.Adr;//从机地址
+    pUser->Value[6] = pCfg->U.S.WaitRoute; //路由等待时间
+    break;
+  }
+  case TM_NOTIFY_SET_DATA:{ //保存设定值
+    pCfg->U.M.Cfg = (pUser->Value[0] << 4) |
+                    (pUser->Value[1] << 2) | 
+                    pUser->Value[2];
+    if(pCfg->U.M.SpaceT != pUser->Value[3])
+      pCfg->U.M.SpaceT = pUser->Value[3];//帧间隔时间
+    else pCfg->U.M.SpaceT = pUser->Value[5];//从机地址
+    
+    if(pCfg->U.M.WaitT != pUser->Value[4])   
+      pCfg->U.M.WaitT = pUser->Value[4]; //接收等待时间
+    else pCfg->U.S.WaitRoute = pUser->Value[6];//路由等待时间 
+    
+    UsartDevCfg_Save(UsartDevCfgTMenu_cbGetId());
+    UiTips_UpdateS(ls_OpFinal); //提示成功
+    break; 
+  }
+  case TM_NOTIFY_MNUM_GET_DESC:{ //得到数值描述
+    memcpy(&pUser->Desc,  &_Desc[pUser->CurItem], 
+           sizeof(struct _MNumDesc));
+    break;
+  }
+  case TM_NOTIFY_USER_GET_LUT:{ //得到查找表
+    const unsigned char *pLUT;
+    if(UsartDevCfgU0_cbIsMasterMask & (1 << (pCfg->U.M.Cfg >> 4))){
+      if(Power_IsSuper()) pLUT = _MasterSuper;
+      else pLUT = _MasterAdmin;
+    }
+    else{
+      if(Power_IsSuper()) pLUT = _SlaveSuper;
+      else pLUT = _SlaveAdmin;    
+    }
+    memcpy(pv, pLUT, *pLUT + 1);
+    break;
+  }
+  default:break;
+  }
+}
+
+//对应的字符
+const LanCode_t* const lsAry_U0[] = {
+  ls_WorkMode, ls_Protocol, ls_ProtocolPara, 
+  ls_SpaceT, ls_WaitT, ls_SlaveAdr, ls_WaitRouter};
+
+//----------------------U0模式的菜单结构-------------------------
+const TMenu_t UsartDevCfgU0_TMenu = {//菜单结构
+  TMTYPE_MNUMADJ | TM_MNUMADJ_WRITE, //菜单类型为多值调整模式模式与用户区标志
+  0x80 | 5,                          //由菜单类型决定的相关数据大小
+  ls_WorkParaSet,                    //菜单头,为NULL时从回调里读取
+  USART_DEV_CFG_TMENU_PARENT,        //自已的父菜单
+  lsAry_U0,                         //存放自已的子菜单阵列连接头
+  _Notify,                           //与用户空间交互的通报函数
+};
+
+
+
+
+
+
--- UsartDevCfgUser0.h
+++ UsartDevCfgUser0.h
@@ -1,6 +1,6 @@
 /******************************************************************************
 
-           UsartDevCfg扩展用户配置->方式0
+                     UsartDevCfg扩展用户配置->方式0
 主要用于主从机时，控制地址与间隔时间等
 ******************************************************************************/
 #ifndef __USART_DEV_CFG_USER0_H
@@ -12,17 +12,31 @@
 //----------------------------用户结构配置--------------------------------------
 typedef union _UsartDevCfgUser0{
   struct{
+    unsigned char Cfg;        //相关配置,见定义
+    unsigned char SpaceT;   //数据帧间隔时间,ms为单位    
     unsigned char WaitT;    //发送数据后的等待时间,ms为单位
-    unsigned char SpaceT;   //数据帧间隔时间,ms为单位
-    unsigned char Cfg;      //主机时配置，如通讯载议等
   }M; //主机模式时
   struct{
+    unsigned char Cfg;        //相关配置，见定义
     unsigned char Adr;        //从机地址
     unsigned char WaitRoute;  //设备做中转合用时，等待下级设备返回时间
-    unsigned char Cfg;        //从机时配置，如通讯载议等
   }S; //从机模式时
 }UsartDevCfgUser0_t;
 
+//-------------------------主从机模式下Cfg配置----------------------------------
+//工作模式：
+#define USART_DEV_UMODE_SHIRT      4     
+#define USART_DEV_UMODE_MASK       0xf0
+#define USART_DEV_UMODE_SLAVE      0     //从机模式
+#define USART_DEV_UMODE_MASTER     1     //主机模式
+                                     //其它如主机,从机,打印机，WIFI等,由用户决定
+
+//工作模式对应通讯协议,如Modbus,透传等,具体由用户决定
+#define USART_DEV_UPROTOCOL_SHIRT  2
+#define USART_DEV_UPROTOCOL_MASK   0xC0 
+
+//工作模式与协议下的配置(如MODBUS下的RTU与ASC)
+#define USART_DEV_UPARA_MASK       0x03 
 
 #endif
 
